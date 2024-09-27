706993ef27bd08dde7410ce9a52f743a86774473
TheEpicMew <whatthesigma914@gmail.com>
2024-09-27 16:11:06 -0700

    Update LuaValue.hpp

--- LuaValue/LuaValue.hpp
+++ LuaValue/LuaValue.hpp
@@ -1,1 +1,29 @@
+#ifndef LUAVALUE_HPP
+#define LUAVALUE_HPP
+
+#include <string>
+
+enum LuaType {
+    LUA_NUMBER,
+    LUA_STRING,
+    LUA_BOOL,
+    LUA_NIL
+};
+
+struct LuaValue {
+    LuaType type;
+    union {
+        double number;
+        bool boolean;
+        std::string* str;
+    } value;
+
+    LuaValue();
+    LuaValue(double num);
+    LuaValue(bool b);
+    LuaValue(const std::string& s);
+    ~LuaValue();
+};
+
+#endif
 
