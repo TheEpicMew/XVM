f45e2eb0a42815dddc3f9a7001257a7f93efce34
TheEpicMew <whatthesigma914@gmail.com>
2024-09-27 19:05:46 -0700

    Update CLVM.cpp

--- CLVM/CLVM.cpp
+++ CLVM/CLVM.cpp
@@ -2,7 +2,25 @@
 #include <sstream>
 #include <iostream>
 
-CLVM::CLVM() {}
+CLVM::CLVM() {
+    initLua();
+}
+
+CLVM::~CLVM() {
+    closeLua();
+}
+
+void CLVM::initLua() {
+    L = luaL_newstate();  //lua state baby
+    luaL_openlibs(L);
+}
+
+void CLVM::closeLua() {
+    if (L) {
+        lua_close(L);    
+        L = nullptr;
+    }
+}
 
 void CLVM::run(const std::string& bytecode) {
     std::istringstream stream(bytecode);
@@ -23,18 +41,31 @@ void CLVM::execute(const std::string& bytecode) {
         iss >> value;
 
         if (value == "true") {
-            stack.push(LuaValue(true));
+            stack.push(LuaValue(true));   
         } else if (value == "false") {
-            stack.push(LuaValue(false));
+            stack.push(LuaValue(false));  
         } else {
-            stack.push(LuaValue(std::stod(value)));
+            stack.push(LuaValue(std::stod(value))); 
         }
     } else if (op == "PRINT") {
         LuaValue val = stack.pop();
         if (val.type == LUA_NUMBER) {
-            std::cout << val.value.number << std::endl;
+            std::cout << val.value.number << std::endl; 
         } else if (val.type == LUA_BOOL) {
             std::cout << (val.value.boolean ? "true" : "false") << std::endl;
         }
+    }
+}
+
+void CLVM::loadLuaScript(const std::string& script) {
+    if (luaL_loadstring(L, script.c_str()) || lua_pcall(L, 0, 0, 0)) {
+        lua_pop(L, 1);
+    }
+}
+
+void CLVM::runLuaFunction(const std::string& functionName) {
+    lua_getglobal(L, functionName.c_str()); 
+    if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+        lua_pop(L, 1);
     }
 }
